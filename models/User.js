84c41dbc2284bcc060b8882646672af3ed4d4cb0
alexisbenavidez99 <callmealexis49@gmail.com>
2023-02-25 12:34:49 -0600

    started user model

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        unique: true,
+        required: true,
+        trimmed: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Please enter a valid email address']
+      }  
+    }
+)
