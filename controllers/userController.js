6e55639114b49f521822c026dc1e2156e90dc8d4
alexisbenavidez99 <callmealexis49@gmail.com>
2023-02-25 16:47:45 -0600

    added user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const { User, Thought } = require('../models');
+const { User } = require('../models');
 
 module.exports = {
     // Get all users
@@ -25,4 +25,70 @@ module.exports = {
           .then((dbUserData) => res.json(dbUserData))
           .catch((err) => res.status(500).json(err));
     },
+    // Update user by id
+    updateUserById(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $set: req.body },
+            { runValidators: true, new: true }
+        )
+        .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user found with this ID' })
+          : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+    },
+    // Delete user by id
+    deleteUserById(req, res) {
+        User.findOneAndDelete({ _id: req.params.userId })
+        .then((user) => {
+            if (!user) {
+                res.status(404).json({ message: 'No user found with this ID' })
+                return;
+            }
+        })
+        .then((user) => {
+            res.json({ message: 'User delete successfully!' })
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        })
+    },
+    // Add friend
+    addFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $push: { friends: req.body } },
+            { runValidators: true, new: true }
+        )
+        .then((user) => {
+            if (!user) {
+                res.status(404).json({ message: 'No user found with that ID' })
+                return;
+            }
+            res.json(user);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        })
+    },
+    // Remove friend
+    removeFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: { userId: req.params.userId } } },
+            { new: true }
+        )
+        .then((user) => {
+            if (!user) {
+                res.status(404).json({ message: 'No user found with this ID' })
+                return;
+            }
+            res.json(user);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        })
+    }
 };
