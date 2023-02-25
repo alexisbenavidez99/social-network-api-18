9adf75e3b8c09d253e7184b5d43854d385f02b83
alexisbenavidez99 <callmealexis49@gmail.com>
2023-02-25 15:19:47 -0600

    added routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+
+const {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUserById,
+    deleteUserById,
+    addFriend,
+    removeFriend,
+} = require('../../controllers/userController');
+
+const {
+    getThoughts,
+    getThoughtById,
+    createThought,
+    updateThoughtById,
+    deleteThoughtById,
+    createReaction,
+    removeReaction,
+} = require('../../controllers/thoughtController');
+
+// User Routes
+
+router.route('/users').get(getUsers).post(createUser);
+router.route('/users/:userId').get(getUserById).put(updateUserById).delete(deleteUserById);
+router.route('/users/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+// Thought Routes
+
+router.route('/thoughts').get(getThoughts).post(createThought);
+router.route('/thoughts/:thoughtId').get(getThoughtById).put(updateThoughtById).delete(deleteThoughtById);
+router.route('/thoughts/:thoughtId/reactions').post(createReaction).delete(removeReaction);
+
+module.exports = router;
+
