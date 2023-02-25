1ad6cb9e5bdcb6c90278d7243d5ae35ce0ea996b
alexisbenavidez99 <callmealexis49@gmail.com>
2023-02-25 16:51:38 -0600

    modified route index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 
-router.use('/user', userRoutes);
+router.use('/', userRoutes);
 
 module.exports = router;
