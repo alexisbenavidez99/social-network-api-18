a800b6254971c085ff9caa72fa12ea6d9a3197d9
alexisbenavidez99 <callmealexis49@gmail.com>
2023-02-27 18:08:17 -0600

    fixed package.json and user controller

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
-const mongodb = require('mongodb').MongoClient;
 
 const app = express();
 const PORT = process.env.PORT || 3001;
